e3ecd62f4124706613fc81474e8dd942a22ce6d0
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2024-04-12 15:35:48 +0900

    Initial Commit

---
+++ WatchIt/WatchItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchItApp.swift
+//  WatchIt
+//
+//  Created by 김지연 on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
